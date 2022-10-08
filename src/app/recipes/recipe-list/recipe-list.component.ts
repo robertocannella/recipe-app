50f5ebe40de0d314281b9644adbb6d665706a81f
robertocannella <robertocannella@gmail.com>
2022-10-07 22:31:33 -0400

    Add Recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
-
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Tasty Italian cuisine!', 'https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222'),
+    new Recipe('Tacos', 'Scrumptious Mexican cuisine!', 'https://st2.depositphotos.com/1006753/8665/i/450/depositphotos_86659340-stock-photo-delicious-tacos.jpg')
+  ];
   ngOnInit(): void {
   }
 
