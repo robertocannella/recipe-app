69eff6aa1223e83a63a80947e2c5ddbdbc29b2e6
robertocannella <robertocannella@gmail.com>
2022-10-10 06:49:49 -0400

    Add FormGroup and FormControls to recipe-edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute,Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,13 +11,32 @@ import { ActivatedRoute,Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id!: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) { }
+  recipeFrom!: FormGroup;
+  constructor(private route: ActivatedRoute,private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((p:Params)=>{
       this.id = +p['id'];
       this.editMode = p['id'] != null;
+      this.initForm();
     })
   }
+  private initForm(){
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeDesc = '';
 
+    if (this.editMode){
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImageUrl = recipe.imageUrl;
+      recipeDesc = recipe.description;
+    }
+
+    this.recipeFrom = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imgUrl': new FormControl(recipeImageUrl),
+      'description': new FormControl(recipeDesc)
+    })
+  }
 }
